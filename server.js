aa07629a12f0314982b6a2bb9de848ea75e506fd
akungu-brian-malowa <bramalowa@gmail.com>
2023-07-17 06:07:58 -0400

    added login api

--- server.js
+++ server.js
@@ -1,9 +1,35 @@
-const express = require("express");
-const app = express();
+const app = require("./app");
+const connectDatabase = require("./db/Database");
+const mongoose = require('mongoose');
 
-app.listen(4000);
+//const PORT = 8000;
+//config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+    require("dotenv").config({
+      path: "backend/config/.env",
+    });
+  }
 
-app.get("/test", (req,res) => {
+//Handling Uncought Exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`shutting down the server for handling uncaught exception`);
+});
 
+//connect db
+connectDatabase();
 
+//create server
+app.listen(process.env.PORT, () => {
+  console.log(`server is running on port: ${process.env.PORT}`);
+});
+
+//unhandled promise rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`shutting down the server for ${err.message}`);
+  console.log(`shutting down the server for handling promise rejection`);
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
